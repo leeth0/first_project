43f99924fd460bfbe6265452d24fc2bb6e65a777
leeth0 <sn2025156@shs.hs.kr>
2025-06-12 11:44:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,10 @@ import random
 st.set_page_config(page_title="오늘의 나를 위한 한마디 🌟", page_icon="💬", layout="centered")
 
 # 스타일 적용
-st.markdown(
-    """
+st.markdown("""
     <style>
     .big-font {
         font-size: 36px !important;
         font-weight: bold;
         color: #6A5ACD;
-        text-shadow: 2px 2px 10px rgba(106, 90, 205, 0.5);
-        text-align: center;
-    }
-    .message-box {
-        border: 3px solid #FFD700;
-        background: #FFF8DC;
-        border
+        text-shadow: 2px 2px 10px rgba(106, 90, 205, 0.5
